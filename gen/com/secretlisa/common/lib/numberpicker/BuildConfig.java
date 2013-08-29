552afaf9e082e000fe9b76f781cac442690450b4
hustwyz <hustwyz@gmail.com>
2013-08-29 15:31:08 +0800

    init

---
+++ gen/com/secretlisa/common/lib/numberpicker/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.secretlisa.common.lib.numberpicker;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
